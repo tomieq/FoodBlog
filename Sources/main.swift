b4beceac0c7329135bc128e7b371d8d2a92a8959
tomieq <tomieq@wp.pl>
2024-09-30 12:56:20 +0200

    Implemented uploading images

--- Sources/main.swift
+++ Sources/main.swift
@@ -14,6 +14,24 @@ do {
         template.body = Template.load(relativePath: "templates/body.tpl.html")
         return .ok(.html(template))
     }
+    server["/admin"] = { request, headers in
+        let template = BootstrapTemplate()
+        template.title = "Jem na mieście"
+        template.addCSS(url: "css/style.css")
+        
+        for multiPart in request.parseMultiPartFormData() {
+            if let fileName = multiPart.fileName {
+                let path = FileManager.default.currentDirectoryPath + "/volume/" + fileName
+                FileManager.default.createFile(atPath: path, contents: Data(multiPart.body), attributes: nil)
+            }
+        }
+
+        let adminTemplate = Template.load(relativePath: "templates/admin.tpl.html")
+        adminTemplate["form"] = Template.cached(relativePath: "templates/uploadForm.tpl.html")
+        
+        template.body = adminTemplate
+        return .ok(.html(template))
+    }
     server.notFoundHandler = { request, responseHeaders in
         // serve Bootstrap static files
         if let filePath = BootstrapTemplate.absolutePath(for: request.path) {
