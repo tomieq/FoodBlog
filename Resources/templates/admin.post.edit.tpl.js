0a3fa741c927c53b5888544c9cc533b9dbbc6f80
tomieq <tomieq@wp.pl>
2024-10-12 10:41:35 +0200

    Removed not working dragsort
    dragging did not update the input's value

--- Resources/templates/admin.post.edit.tpl.js
+++ Resources/templates/admin.post.edit.tpl.js
@@ -13,14 +13,6 @@ $( function() {
     var tagifyPhotos = new Tagify(inputPhoto, {
         originalInputValueFormat: valuesArr => valuesArr.map(item => item.value).join(',')
     });
-    // bind "DragSort" to Tagify's main element and tell
-    // it that all the items with the below "selector" are "draggable"
-    var dragsort = new DragSort(tagifyPhotos.DOM.scope, {
-        selector: '.'+tagify.settings.classNames.tag,
-        callbacks: {
-            dragEnd: onDragEnd
-        }
-    })
 
     // must update Tagify's value according to the re-ordered nodes in the DOM
     function onDragEnd(elm){
